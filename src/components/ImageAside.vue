6009ef771dcbc93956cf1e0e613f9bddb38180d3
Brief <brf2053@gmail.com>
2026-01-06 01:59:59 +0800

    finish 6.3

---
+++ src/components/ImageAside.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ElButton } from 'element-plus';
+import MyComponents from './MyComponents.vue';
+import { Close, Edit } from '@element-plus/icons-vue';
+import AsideList from '~/components/AsideList.vue';
+
+</script>
+
+<template>
+    <el-aside width="220px" class="image-aside">
+        <div class="top">
+            <aside-list active>
+                分类标题
+            </aside-list>
+            <aside-list >
+                分类标题
+            </aside-list>
+        </div>
+        <div class="bottom">
+            分页区域
+        </div>
+
+    </el-aside>
+</template>
+
+<style>
+.image-aside{
+    border-right: 1px solid #eeeeee;
+    position: relative;
+}
+.image-aside .top{
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 50px;
+    overflow-y: auto;
+}
+
+.image-aside .bottom{
+    position: absolute;
+    bottom: 0;
+    height: 50px;
+    left: 0;
+    right: 0;
+
+    @apply flex items-center justify-center
+}
+
+</style>
---
+++ src/components/MyComponents.vue
@@ -0,0 +1,33 @@
+<<template>
+    <div class="outer">
+        <span class="m-4">
+            运动旅行
+        </span>
+        <div class="ttt">
+            <el-icon class="m-2 cursor-pointer text-4xl text-blue-400">
+                <Edit />
+                <Close />
+            </el-icon>
+            <!-- <el-icon class="m-2 cursor-pointer">
+                
+            </el-icon> -->
+        </div>
+    </div>
+
+</template>
+
+
+<style>
+.outer:hover{
+    background-color: aliceblue;
+}
+.outer{
+    
+    height: 50px;
+    @apply rounded flex items-center
+}
+    .ttt {
+        display: inline;
+        @apply ml-auto 
+    }
+</style>
